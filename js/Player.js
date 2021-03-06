666af6a67d2fb88610fa4f67852ab90eda2f2dc4
qwerty819 <74083220+qwerty819@users.noreply.github.com>
2021-03-05 20:40:32 -0500

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,67 @@
+class Player{
+    constructor(){
+        this.name = null;
+        this.distance = 0;
+        this.index = null;
+        this.rank = null;
+    }
+    getCount(){
+        var playerCountRef = database.ref("playerCount");
+        playerCountRef.on("value",function(data){
+            playerCount=data.val();
+        })
+    }
+    updateCount(count){
+        database.ref("/").update({
+            playerCount:count
+        })
+    }
+
+    updateName(){
+        var playerIndex = "players/player"+this.index;//player+1=player1
+        database.ref(playerIndex).set({
+            name:this.name  ,
+            distance:this.distance 
+        })
+    }
+   //static functions are the functions who dont need object to be called its common for everyone in class
+
+   static getPlayerInfo(){
+        var playerInfoRef = database.ref("players");
+        playerInfoRef.on("value",function(data){
+            allPlayers = data.val();
+        })
+    }
+
+    getCarsAtEnd(){
+        var carsAtEndRef = database.ref("carsAtEnd");
+        carsAtEndRef.on("value",function(data){
+            this.rank = data.val();
+        })
+    }
+
+    static updateCarsAtEnd(rank){
+        database.ref("/").update({
+            carsAtEnd:rank
+        })
+    }
+}
+
+//abc-1, def-3,rgt-2,xzy-4
+//player1-abc
+//player2-rgt
+//palyer3-def
+
+//players 
+    //player1
+        //name
+        //distance    
+
+    //player2
+    //player3
+    //player4
+
+    //allplayers=[player1,player2,player3,player4]
+    //allpalyer={player1:{name:abc,distance:50},
+    //          player2:{name:abc,distance:50},
+    //          player3:{name:abc,distance:50}}
